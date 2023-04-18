8236c32d07449a2ecb59ede8d6b8b6e7f42045d4
iTarley <nikuradzeluka1@gmail.com>
2023-04-18 19:46:00 +0400

    DiffUtil fixes ?

--- app/src/main/java/com/example/chatapp_spaceintern/domain/model/MessageModel.kt
+++ app/src/main/java/com/example/chatapp_spaceintern/domain/model/MessageModel.kt
@@ -1,19 +1,8 @@
 package com.example.chatapp_spaceintern.domain.model
 
-import com.example.chatapp_spaceintern.presentation.base.BaseDiff
-
 data class MessageModel(
     val id: Int?,
     val sender: String?,
     val message: String?,
     val time: String?
-): BaseDiff<MessageModel>() {
-    override val inner: MessageModel
-        get() = this
-    override val uniqueValue: Any
-        get() = id ?: ""
-
-    override fun compareTo(other: Any?): Boolean {
-        return other is MessageModel && this == other
-    }
-}
+)
--- app/src/main/java/com/example/chatapp_spaceintern/presentation/base/BaseDiff.kt
+++ app/src/main/java/com/example/chatapp_spaceintern/presentation/base/BaseDiff.kt
@@ -1,29 +1,16 @@
 package com.example.chatapp_spaceintern.presentation.base
 
+import android.annotation.SuppressLint
 import androidx.recyclerview.widget.DiffUtil
 
-abstract class BaseDiff<T> {
-
-    abstract val inner: T
-    abstract val uniqueValue: Any
-    abstract fun compareTo(other: Any?): Boolean
-    override fun equals(other: Any?): Boolean {
-        return compareTo(other)
-    }
-
-    override fun hashCode(): Int {
-        var result = inner?.hashCode() ?: 0
-        result = 31 * result + uniqueValue.hashCode()
-        return result
-    }
-}
+class DiffCallback<T : Any> : DiffUtil.ItemCallback<T>() {
 
 
-class DiffCallback<T : BaseDiff<T>> : DiffUtil.ItemCallback<T>() {
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean {
-        return oldItem.uniqueValue == newItem.uniqueValue
+        return oldItem.hashCode() == newItem.hashCode()
     }
 
+    @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean {
         return oldItem == newItem
     }
