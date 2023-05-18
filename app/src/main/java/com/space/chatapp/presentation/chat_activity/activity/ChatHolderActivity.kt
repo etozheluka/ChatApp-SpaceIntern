07dd29c8dbe310a9f44b0dbd5d3ee9a060631f70
iTarley <nikuradzeluka1@gmail.com>
2023-05-18 18:05:55 +0400

    chat with viewpager

--- app/src/main/java/com/space/chatapp/presentation/chat_activity/activity/ChatHolderActivity.kt
+++ app/src/main/java/com/space/chatapp/presentation/chat_activity/activity/ChatHolderActivity.kt
@@ -1,44 +1,30 @@
 package com.space.chatapp.presentation.chat_activity.activity
 
-import android.content.Context
-import android.content.res.Configuration
 import android.os.Bundle
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.space.chatapp.R
 import com.space.chatapp.databinding.ActivityMainBinding
 import com.space.chatapp.presentation.chat_activity.viewmodel.ChatHolderViewModel
 import com.space.chatapp.presentation.chat_screen.ui.fragment.ChatFragment
 import com.space.chatapp.utils.ChatThemeMode
-import com.space.chatapp.utils.extension.*
+import com.space.chatapp.utils.extension.getDrawable
+import com.space.chatapp.utils.extension.launchWithLifecycle
+import com.space.chatapp.utils.extension.viewBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ChatHolderActivity : AppCompatActivity() {
     private val binding by viewBinding(ActivityMainBinding::inflate)
     private val viewModel by viewModel<ChatHolderViewModel>()
-    private val containers = ArrayList<Int>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         hideNavBar()
-        if (savedInstanceState != null) {
-            val savedContainerIds = savedInstanceState.getIntegerArrayList(CONTAINERS_STATE_KEY)
-            if (savedContainerIds != null) {
-                containers.addAll(savedContainerIds)
-            }
-        }
-        initFragment(5)
+        initFragment(3)
         observeThemeMode()
         checkPreferencesStatus()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val containerIds = ArrayList<Int>(containers)
-        outState.putIntegerArrayList(CONTAINERS_STATE_KEY, containerIds)
     }
 
     private fun checkPreferencesStatus() {
@@ -72,34 +58,16 @@ class ChatHolderActivity : AppCompatActivity() {
      * Initialize fragment container
      */
     private fun initFragment(containerCount: Int) {
-        with(binding) {
-            containers.clear()
-            val totalWeight = containerCount.toFloat()
+        val fragmentList = mutableListOf<Fragment>()
+        val fragmentTitleList = mutableListOf<String>()
 
-            for (i in 0 until containerCount) {
-                val frameLayout = createFrameLayout()
-                containers.add(frameLayout.id)
-
-                val layoutParams = createLayoutParams(totalWeight, binding.root.context)
-                fragmentContainer.addView(frameLayout, layoutParams)
-
-                val divider = createDivider()
-                val dividerLayoutParams = createDividerLayoutParams(binding.root.context)
-                fragmentContainer.addView(divider, dividerLayoutParams)
-            }
-            containers.forEachIndexed { index, frameLayout ->
-                val fragment by lazy { createChatFragment(index) }
-                val bundle by lazy { createBundle(index) }
-                fragment.arguments = bundle
-                setFragmentToContainer(frameLayout, fragment)
-            }
+        for (i in 0 until containerCount) {
+            val fragment = createChatFragment(i)
+            fragmentList.add(fragment)
+            fragmentTitleList.add("Fragment ${i + 1}")
         }
-    }
 
-    private fun createFrameLayout(): FrameLayout {
-        val frameLayout = FrameLayout(binding.root.context)
-        frameLayout.id = View.generateViewId()
-        return frameLayout
+        binding.viewPager?.adapter = ViewPagerFragmentAdapter(this@ChatHolderActivity, fragmentList)
     }
 
     private fun createChatFragment(index: Int): ChatFragment {
@@ -109,58 +77,6 @@ class ChatHolderActivity : AppCompatActivity() {
         return fragment
     }
 
-    private fun createDivider(): View {
-        val divider = View(binding.root.context)
-        divider.setBackgroundColorRes(R.color.dark_yellow200)
-        return divider
-    }
-    /**
-     * Create layout params for fragment container
-     */
-    private fun createLayoutParams(
-        totalWeight: Float,
-        context: Context
-    ): LinearLayout.LayoutParams {
-        val orientation = context.resources.configuration.orientation
-        val layoutParams: LinearLayout.LayoutParams =
-            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                // Landscape mode parameters
-                LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    1f / totalWeight
-                )
-            } else {
-                // Portrait or other mode parameters
-                LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    0,
-                    1f / totalWeight
-                )
-            }
-        return layoutParams
-    }
-    /**
-     * Create divider layout params
-     */
-    private fun createDividerLayoutParams(context: Context): LinearLayout.LayoutParams {
-        val orientation = context.resources.configuration.orientation
-        val layoutParams: LinearLayout.LayoutParams =
-            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
-                // Landscape mode parameters
-                LinearLayout.LayoutParams(
-                    6,
-                    LinearLayout.LayoutParams.MATCH_PARENT
-                )
-            } else {
-                // Portrait or other mode parameters
-                LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    6
-                )
-            }
-        return layoutParams
-    }
     /**
      * Create bundle for fragment
      */
@@ -169,6 +85,7 @@ class ChatHolderActivity : AppCompatActivity() {
         bundle.putString(USER_ID, "fragment_${index + 1}")
         return bundle
     }
+
     companion object {
         private const val CONTAINERS_STATE_KEY = "containers_state_key"
         private const val USER_ID = "userId"
---
+++ app/src/main/java/com/space/chatapp/presentation/chat_activity/activity/FragmentPagerAdapter.kt
@@ -0,0 +1,20 @@
+package com.space.chatapp.presentation.chat_activity.activity
+
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerFragmentAdapter(
+    fragmentActivity: FragmentActivity,
+    private val fragmentList: List<Fragment>
+) : FragmentStateAdapter(fragmentActivity) {
+
+    override fun getItemCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+}
